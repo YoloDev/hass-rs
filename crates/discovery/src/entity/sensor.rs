ad26040f7a85a359f32b5b011508300af9da5be3
arcnmx <git@git.arcn.mx>
2022-09-30 13:55:49 -0700

    feat: add `availability` to `Sensor` entity

--- crates/discovery/src/entity/sensor.rs
+++ crates/discovery/src/entity/sensor.rs
@@ -1,5 +1,5 @@
 use crate::{
-  device::Device,
+  availability::{Availability, AvailabilityMode}, device::Device,
   device_class::DeviceClass, entity_category::EntityCategory, icon::Icon, name::Name, qos::MqttQoS,
   state_class::StateClass, template::Template, topic::Topic, unique_id::UniqueId,
 };
@@ -14,6 +14,26 @@ use std::{borrow::Cow, num::NonZeroU32};
 /// See: <https://www.home-assistant.io/integrations/sensor.mqtt/>
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Sensor<'a> {
+  /// A list of MQTT topics subscribed to receive availability (online/offline) updates.
+  /// Must not be used together with `availability_topic`.
+  #[serde(borrow, default, skip_serializing_if = "<[Availability]>::is_empty")]
+  pub availability: Cow<'a, [Availability<'a>]>,
+
+  /// When `availability` is configured, this controls the conditions needed
+  /// to set the entity to `available`.
+  #[serde(default, skip_serializing_if = "AvailabilityMode::is_default")]
+  pub availability_mode: AvailabilityMode,
+
+  /// Defines a template to extract device’s availability from the `availability_topic`.
+  /// To determine the devices’s availability result of this template will be compared
+  /// to `payload_available` and `payload_not_available`.
+  #[serde(borrow, default, skip_serializing_if = "Option::is_none")]
+  pub availability_template: Option<Template<'a>>,
+
+  /// The MQTT topic subscribed to receive availability (online/offline) updates.
+  #[serde(borrow, default, skip_serializing_if = "Option::is_none")]
+  pub availability_topic: Option<Topic<'a>>,
+
   /// Information about the device this sensor is a part of to tie it into the device registry.
   /// Only works through MQTT discovery and when `unique_id` is set.
   /// At least one of identifiers or connections must be present to identify the device.
@@ -81,6 +101,16 @@ pub struct Sensor<'a> {
   /// Used instead of `name` for automatic generation of `entity_id`.
   #[serde(borrow, default, skip_serializing_if = "Option::is_none")]
   pub object_id: Option<Cow<'a, str>>,
+
+  /// The payload that represents the available state.
+  /// Defaults to `"online"`
+  #[serde(borrow, default, skip_serializing_if = "Option::is_none")]
+  pub payload_available: Option<Cow<'a, str>>,
+
+  /// The payload that represents the unavailable state.
+  /// Defaults to `"offline"`
+  #[serde(borrow, default, skip_serializing_if = "Option::is_none")]
+  pub payload_not_available: Option<Cow<'a, str>>,
 
   /// The maximum QoS level of the state topic.
   #[serde(default, skip_serializing_if = "MqttQoS::is_default")]
